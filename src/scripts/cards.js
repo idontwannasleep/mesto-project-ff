3b7cce43c8186a0d011219dae004b6d9e9374c4d
Denis <prosvirnin26@gmail.com>
2025-04-28 23:46:07 +0300

    фикс

---
+++ src/scripts/cards.js
@@ -0,0 +1,26 @@
+export const initialCards = [
+    {
+      name: "Калининград",
+      link: "https://images.unsplash.com/photo-1629104411440-7ebc9134f93e?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Сочи",
+      link: "https://images.unsplash.com/photo-1599827206955-018384540f40?q=80&w=3642&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Санкт-Петербург",
+      link: "https://images.unsplash.com/photo-1609524400436-2226c04f8a84?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Камчатка",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+      name: "Холмогорский район",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+      name: "Байкал",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    }
+];
